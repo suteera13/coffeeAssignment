cc1cf3a849f73f7c47c96045e7a90db6ed9ee022
suteera13 <39986569+suteera13@users.noreply.github.com>
2023-03-15 17:06:20 +0700

    update login.js and class.php

--- api/class.php
+++ api/class.php
@@ -1,8 +1,11 @@
 <?php
     include_once 'connect.php';
     class control extends db{
-        public function checkUser($id,$name,$pass){
-            
+        public function checkUser($name,$pass){
+            $sql = "SELECT * FROM user WHERE user_name = {$name} 
+                and user_pass = {$pass}";
+            $check = $this->db->query($sql)->fetchall(PDO::FETCH_ASSOC);
+            return $check;
         }
     }
 ?>
--- view/js/login.js
+++ view/js/login.js
@@ -1,4 +1,23 @@
-function loginClick(){
-    let user = document.getElementById("username").value
-    let pass = document.getElementById("password").value
+async function loginClick(){
+    
+    url = "http://localhost/coffeeAssignment/api/class.php";
+    const checkUser = {
+        user_name : document.getElementById("username").value,
+        user_pass : document.getElementById("password").value
+    };
+    try{
+        const response = await fetch(url,{
+            method: "post",
+            headers: {"Content-Type":"application/text"},
+            body: JSON.stringify(checkUser)
+        });
+        if(!response.ok){
+            const message = "Error with status code : "+ response.status;
+            throw new Error(message);
+        }
+        const data = await response.text();
+        console.log(data);
+    }catch(er){
+        console.log("Error : "+ er);
+    }
 }
