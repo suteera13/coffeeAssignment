a8503c8f0953eddb28df0c7f654b676721fb3ea9
suteera13 <39986569+suteera13@users.noreply.github.com>
2023-03-21 00:16:48 +0700

    update php and del js

--- api/controls.php
+++ api/controls.php
@@ -1,36 +1,37 @@
+<script>
+    function goto(){
+        setInterval(() => {
+            window.location.href = "../view/Menu.php";
+        }, 1000);
+    }
+</script>
 <?php
     session_start();
     include_once 'class.php';
     $clCon = new control();
-    
-    $data = file_get_contents("php://input");
-    $arr = json_decode($data, true);
 
+    print_r($_POST);
     if($_GET["ac"]==0){
         echo "login\n";
-        $user = $arr["user_name"];
-        $pass = $arr["user_pass"];
-        echo "user: ".$user."\npass: ".$pass."\n";
-        $status = $clCon->checkUser($user,$pass);
-        if($status==1){
-            echo json_encode(["status"=>"ok", "message"=>"welcome to coffee site"]);
-            $_SESSION['userses'] = ["user"=>$user,"pass"=>$pass];
-        }else{
-            echo json_encode(["status"=>"error", "message"=>"login fail"]);
-        }
+        // echo "<script>goto()</script>";
+        // // $user = $arr["user_name"];
+        // // $pass = $arr["user_pass"];
+        // echo "user: ".$user."\npass: ".$pass."\n";
+        // $status = $clCon->checkUser($user,$pass);
+        // if($status==1){
+        //     $_SESSION['userses'] = ["user"=>$user,"pass"=>$pass];
+        // }else{
+        // }
     }
-    else if($_GET["ac"]==1){
-        echo "signin\n";
-        $user = $arr["user_name"];
-        $pass = $arr["user_pass"];
-        echo "user: ".$user." pass: ".$pass;
-        $status = $clCon->addUser($user,$pass);
-        if($status==1){
-            echo json_encode(["status"=>"ok", "message"=>"loading login"]);
-            $_SESSION['userses'] = ["user"=>$user,"pass"=>$pass];
+    // else if($_GET["ac"]==1){
+    //     echo "signin\n";
+    //     $user = $arr["user_name"];
+    //     $pass = $arr["user_pass"];
+    //     echo "user: ".$user." pass: ".$pass;
+    //     $status = $clCon->addUser($user,$pass);
+    //     if($status==1){
             
-        }else{
-            echo json_encode(["status"=>"error", "message"=>"signup fail"]);
-        }
-    }
+    //     }else{
+    //     }
+    // }
 ?>
