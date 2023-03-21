82aa26fea5532ccb4f9ddfa520d849b57966a91d
suteera13 <39986569+suteera13@users.noreply.github.com>
2023-03-21 18:28:56 +0700

    update show menu

--- api/class.php
+++ api/class.php
@@ -7,11 +7,6 @@
         public function checkUser($name,$pass){
             $sql = "SELECT user_id FROM user WHERE user_name = '{$name}' AND user_pass = '{$pass}'";
             return $this->db->query($sql)->fetchall(PDO::FETCH_ASSOC);
-            // $stmt->bindParam(':username', $name , PDO::PARAM_STR);
-            // $stmt->bindParam(':password', $pass , PDO::PARAM_STR);
-            // if($stmt->rowCount() == 1){
-                // return 1;
-            // }
         }
         public function addUser($name,$pass){
             $sql = "INSERT INTO `user`(`user_name`, `user_pass`) VALUES (:username, :password)";
@@ -23,7 +18,6 @@
                 return 1;
             }
         }
-<<<<<<< HEAD
         public function buyOrder($user, $menu, $order){
             $sql = "INSERT INTO `order`(`user_id`, `menu_id`, `order_amount`) VALUES ()";
             $stmt = $this->db->prepare($sql);
@@ -34,12 +28,10 @@
             if($stmt->rowCount() == 1){
                 return 1;
             }
-=======
         public function showMenu(){
             $sql = "SELECT `menu_name`, `menu_price` FROM `menu`";
-            $stmt = $this->db->query($sql);
+            $stmt = $this->db->query($sql)->fetchall(PDO::FETCH_ASSOC);
             // return $stmt;
->>>>>>> d680555b6032f5b492ba10a54124d7260edc7931
         }
     }
 ?>
