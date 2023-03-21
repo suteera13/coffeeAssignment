4981ca1c790b4c98e0caf94f79fa91fba15cb704
suteera13 <39986569+suteera13@users.noreply.github.com>
2023-03-21 20:00:05 +0700

    Update logout.php

--- view/logout.php
+++ view/logout.php
@@ -1,7 +1,11 @@
 <script>
+    alert('logout?');
     function goto(url){
         window.location.href = url;
-}
+    }
 </script>
 <?php
-echo "<script>alert('logout?');goto('../view')</script>";
+    session_start();
+    $_SESSION[] = [];
+    echo "<script>goto('../view')</script>";
+?>
